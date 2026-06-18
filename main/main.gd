546c9a8ed3737d0d77cf53476e0da9eda5c492d2
ethan <ethan.tang@coconutislandstudio.com>
2026-06-18 12:43:21 +0800

    code optimize

--- main/main.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-const GameMcpDemoHandlerScript := preload("res://main/game_mcp_demo_handler.gd")
-
-
-func _ready() -> void:
-	GameMcp.register_handle(GameMcpDemoHandlerScript.new())
--- mcp_handler.gd
+++ mcp_handler.gd
@@ -1,7 +1,9 @@
-extends RefCounted
+extends Node
 
 var command := "ping"
 
+func _ready():
+	GameMcp.register_handle(self)
 
 func on_receive(data: Dictionary, return_callback: Callable) -> void:
 	return_callback.call({
