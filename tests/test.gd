d72d50cab56b516c85bbd40221f20f2b2c2a3aec
ethan <ethan.tang@coconutislandstudio.com>
2026-06-18 12:06:13 +0800

    code optimize

--- tests/test.gd
+++ tests/test.gd
@@ -1,6 +1,17 @@
 class_name Test
 extends SceneTree
 
+static func has_autotest_argument() -> bool:
+	var user_arguments: PackedStringArray = OS.get_cmdline_user_args()
+	return user_arguments.has("--autotest")
+
+static func read_autotest_name_from_command_line() -> String:
+	var user_arguments: PackedStringArray = OS.get_cmdline_user_args()
+	for argument_index in user_arguments.size():
+		if user_arguments[argument_index] == "--autotest" and argument_index + 1 < user_arguments.size():
+			return user_arguments[argument_index + 1]
+	return ""
+
 func _init() -> void:
 	if not has_autotest_argument():
 		return
@@ -10,23 +21,6 @@ func _init() -> void:
 		quit(1)
 		return
 	run_named(test_name)
-
-
-static func has_autotest_argument() -> bool:
-	var user_arguments: PackedStringArray = OS.get_cmdline_user_args()
-	return user_arguments.has("--autotest")
-
-
-static func read_autotest_name_from_command_line() -> String:
-	var user_arguments: PackedStringArray = OS.get_cmdline_user_args()
-	for argument_index in user_arguments.size():
-		if user_arguments[argument_index] == "--autotest" and argument_index + 1 < user_arguments.size():
-			return user_arguments[argument_index + 1]
-	return ""
-
-
-func _run() -> void:
-	print("run")
 
 func run_named(test_name: String) -> void:
 	match test_name:
